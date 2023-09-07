68266be1f2c18da9700e038fe840f198f61e7add
koyoungsan <144293477+koyoungsan@users.noreply.github.com>
2023-09-07 15:59:23 +0900

    ssatdf

--- Project2/FileName.c
+++ Project2/FileName.c
@@ -4,18 +4,17 @@ int main()
 {
 	int a;
 	scanf_s("%d", &a);
-
 	for (int i = 1; i <= a; i++)
 	{
 		for (int j = i; j <= a; j++)
 		{
 			printf(" ");
 		}
-		for (int j = i; j <= i*2-1; j++)
+		for (int j = 1; j <= i * 2 - 1;j++)
 		{
 			printf("*");
 		}
 		printf("\n");
 	}
-	return 0;
+		return 0;
 }
